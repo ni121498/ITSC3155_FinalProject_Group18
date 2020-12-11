c28a0fed07d87403148d82893aae3ef5118ff0e4
Niara Parker <nparke11@uncc.edu>
2020-12-10 20:41:21 -0500

    Update 12/10/20

--- Source Code/internationWealthVisualizer.py
+++ Source Code/internationWealthVisualizer.py
@@ -5,8 +5,13 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 
+# collecting data from GDP.csv
 df = pd.read_csv('../Data_Sets/GDP.csv')
 
+# creating app variable for our dashboard
+app = dash.Dash()
+
+# creating interactive map visual
 fig = go.Figure(data = go.Choropleth(
     locations = df['Country Code'],
     z = df['2019 [YR2019]'],
@@ -20,6 +25,7 @@ fig = go.Figure(data = go.Choropleth(
     colorbar_title='GDP<br>US$',
 ))
 
+# creating layout of the graph
 fig.update_layout(
     title_text='2019 Global GDP',
     geo=dict(
@@ -37,4 +43,66 @@ fig.update_layout(
     )
                    ]
 )
-fig.write_html('world.html', auto_open = True)
+
+# global GDP interactive map
+data_GDP = [
+    go.Choropleth(
+        locations=df['Country Code'],
+        z=df['2019 [YR2019]'],
+        text=df['Country Name'],
+        colorscale= 'Blues',
+        autocolorscale= False,
+        reversescale= True,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        colorbar_tickprefix= '$',
+        colorbar_title='GDP<br>US$',
+    )
+]
+
+# creating layout of website
+app.layout= html.Div(children=[
+    html.H1(children= 'International Wealth Visualizer',
+            style={
+                'textAlign' : 'center',
+                'color' : '#ADA5A3'
+            }
+            ),
+    html.Div('A tool that allows you to be able to view the wealth of the world and provides', style={'textAlign' : 'center'}),
+    html.Div('you with info regarding GDP, GNI, Cost of Living, and Currency', style={'textAlign' : 'center'}),
+    html.Br(),
+    html.Br(),
+    html.Div([
+        dcc.Tabs([
+            dcc.Tab(label='Interactive Map', children=[
+                html.H3('Interactive Map', style={'color' : '#9CB4C6'}),
+                dcc.Graph(
+                    id='Graph1',
+                    figure={
+                        'data' : data_GDP,
+                        'layout' : go.Layout(
+                            title= 'Global GDP in 2019'
+                        )
+
+                    }
+                )
+
+            ]),
+            dcc.Tab(label='Currency Converter', children=[
+
+            ]),
+            dcc.Tab(label='My Page', children=[
+
+            ])
+        ]
+
+        )
+    ]
+
+    )
+
+]
+)
+#fig.write_html('world.html', auto_open = True)
+if __name__ == '__main__':
+    app.run_server()
